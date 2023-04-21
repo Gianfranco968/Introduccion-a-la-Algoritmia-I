a36e677afa10413c19e13516c4bc40dec9566b86
Gian Mazzei <71911178+Gianfranco968@users.noreply.github.com>
2023-04-20 18:31:30 -0600

    Delete ej2.py

--- ej2.py
+++
@@ -1,8 +0,0 @@
-"""Leer un número entero e imprimir un mensaje indicando si es par o impar"""
-def calculo():
-    a = int(input("Introducir un número: "))
-    if a % 2 == 0:
-        print("El número", a, "es par")
-    else:
-        print("El número", a, "es impar")
-calculo()
