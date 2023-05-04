51d6bf6db179efc8a25877d4cb5e408662702692
Gian Mazzei <71911178+Gianfranco968@users.noreply.github.com>
2023-05-04 08:27:00 -0600

    Version 1.0

--- Unidad 5/ejercicio_5 (incompleto).py
+++ Unidad 5/ejercicio_5 (incompleto).py
@@ -11,19 +11,28 @@ def calendario():
         if M == 4 or M == 6 or M == 8 or M == 11:
             if D <= 30 and D >= 1:
                 D = D + N
-                if D < 30:
-                    print("Dia:",D,"Mes:",M,"Año",A)
+                if D > 30:
+                    resto = D - 30
+                    D -= D
+                    reinicio = resto
+                    M = M + 1
+                    print("Dia:",reinicio,"Mes:",M,"Año",A)
         if M == 2:
             if D <= 28 and D >= 1:
                 D = D + N
             if D > 28:
-                resto = D - N
-                reinicio = N + resto
+                resto = D - 28
+                D -= D
+                reinicio = N + D + resto
                 M = M + 1
                 print("Dia:",reinicio,"Mes:",M,"Año",A)
-            else:
-                print("Dia:",D,"Mes:",M,"Año",A)
-calendario()
-                
-                
-    
+        else:
+            if D <= 31 and D >= 1:
+                D = D + N
+                if D > 31:
+                    resto = D - 31
+                    D -= D
+                    reinicio = resto
+                    M = M + 1
+                    print("Dia:",reinicio,"Mes:",M,"Año",A)
+calendario()    
